afca4245c6560ac33194816d570be3b2a2e9111b
Marcus <814712074@qq.com>
2023-08-21 17:47:35 +0800

    chore： 插件注释

--- plugins/updateProjectConfig/index.ts
+++ plugins/updateProjectConfig/index.ts
@@ -8,52 +8,53 @@ export default function updateAppId() {
   if (!TARO_ENV) return;
 
   const envs = ["dev", "test", "prod"];
+  //  获取构建环境参数，默认 dev
   const env = (
     process.argv.find((item) => envs.map((env) => `--${env}`).includes(item)) ||
     "dev"
   ).replace("--", "");
 
+  // appid 的配置写在环境配置文件下
   const envConfig = require(`../../config/${env}.ts`);
+  // ["filename","appid"]的格式
   const appIdOpts = get(envConfig, `appId.${TARO_ENV}`);
   if (!appIdOpts) return;
 
-  console.log("appIdOpts", appIdOpts);
-
   const [targetFile, appid] = appIdOpts;
 
-  // 写入配置
-  // 只更新TARO_ENV 对应文件
+  /**
+   * 写入配置只更新TARO_ENV 对应文件
+   */
+  // base config 文件路径
   const baseConfigPath = path.resolve(__dirname, "../../", "project.base.json");
+  // 自定义 config 文件路径，.conf.json结尾，这个文件不会被复写
   const targetCustomPath = path.resolve(
     __dirname,
     "../../",
     `project.${targetFile || "config"}.conf.json`
   );
-  // console.log(targetCustomPath);
-  // console.log(glob.globSync(targetCustomPath));
+
+  // 检查有没有自定义配置文件
   const hasCustom = !!glob.globSync(targetCustomPath).length;
 
   try {
+    // 1. 先获取 base 配置文件
     const projectConfig =
       JSON.parse(readFileSync(baseConfigPath, "utf-8")) || {};
-
+    // 2. 获取自定义配置文件
     let customConfig = {};
-    if (hasCustom) {
-      // 有自定义配置吗？
+    if (hasCustom)
       customConfig = JSON.parse(readFileSync(targetCustomPath, "utf-8")) || {};
-    }
-
-    // compose
+    // 3. compose
     const config = merge({}, projectConfig, customConfig);
-    // update
+    // 4. update
     config.appid = appid;
-
+    // 5. 写入
     const outputPath = path.resolve(
       __dirname,
       "../../",
       `project.${targetFile}.json`
     );
-
     writeFileSync(outputPath, JSON.stringify(config, null, "\t"));
   } catch (e) {
     console.error("project配置更新执行异常：", e);
