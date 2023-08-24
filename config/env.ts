8530cfd2d9c7c9083fceaea7c10c9c905042f5fa
Marcus <814712074@qq.com>
2023-08-24 17:32:17 +0800

    feat(XXHXT-261): chore

--- config/env.ts
+++ config/env.ts
@@ -2,12 +2,13 @@ const _ = require("lodash");
 
 // 获取环境变量
 module.exports = function getEnvConfig() {
-  const env = (() => {
-    if (process.env.NODE_ENV === "development") {
-      return _.merge({}, require("./dev"));
-    }
-    return _.merge({}, require("./prod"));
-  })().env;
+  const env =
+    (() => {
+      if (process.env.NODE_ENV === "development") {
+        return _.merge({}, require("./dev"));
+      }
+      return _.merge({}, require("./prod"));
+    })().env || {};
 
   Object.entries(env).forEach(([key, value]) => {
     if (typeof value === "string") {
